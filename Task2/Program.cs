3d2d139e092c4f8b18d403572bece294a86c7314
Polina <Polina_Piter88@mail.ru>
2022-10-11 09:07:16 +0300

    2 задача

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+void GetDistanceBetweenTwoPoint(int firstx, int firsty, int firstz,  int secondx, int secondy, int secondz)
+{
+
+    double result = 0;
+    double tempx = Math.Pow(secondx - firstx, 2);
+    double tempy = Math.Pow(secondy-firsty, 2);
+    double tempz = Math.Pow(secondz-firstz, 2);
+    
+    result = Math.Sqrt(tempx + tempy + tempz);
+    
+    Console.WriteLine($"Расстояние между точками равно {result}");
+}
+GetDistanceBetweenTwoPoint(7,-5,0,1,-1,9); 
